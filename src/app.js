fe5ee021233724d155565e99e44eb111f64f11be
Edunga1 <Edunga1@users.noreply.github.com>
2024-01-30 15:51:50 +0000

    deploy: 392b5310916e5e1e635680e0ffcc6f0376ba3d1e

---
+++ src/app.js
@@ -0,0 +1,127 @@
+class Canvas {
+  constructor(
+    document,
+    container,
+    width,
+    height,
+  ) {
+    this.document = document
+    this.container = container
+    this.canvas = this.#createCanvas(width, height)
+    this.container.appendChild(this.canvas)
+    /** @type {CanvasRenderingContext2D} */
+    this.context = this.canvas.getContext("2d")
+  }
+
+  get width() {
+    return this.canvas.width
+  }
+
+  get height() {
+    return this.canvas.height
+  }
+
+  #createCanvas(width, height) {
+    const canvas = this.document.createElement("canvas")
+    canvas.width = width
+    canvas.height = height
+    return canvas
+  }
+
+  resize(width, height) {
+    this.canvas.width = width
+    this.canvas.height = height
+  }
+
+  addText(text, x, y, size = 20) {
+    this.context.fillStyle = "white"
+    this.context.font = `${size}px Arial`
+    this.context.fillText(text, x, y)
+  }
+
+  draw(func) {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.context.fillStyle = "black"
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    this.context.save()
+    func(this)
+    this.context.restore()
+  }
+}
+
+class World {
+  constructor(canvas) {
+    this.velocityRange = 0.1
+    this.canvas = canvas
+    this.word = ''
+    this.entities = [
+      {
+        c: '!',
+        x: 100,
+        y: 100,
+        size: 20,
+        velocity: {x: 0, y: 0},
+      },
+    ]
+  }
+
+  run(word = 'unknown') {
+    const that = this
+    this.word = word
+    this.#initWord()
+
+    requestAnimationFrame(function tick() {
+      that.#update()
+      that.#render()
+      requestAnimationFrame(tick)
+    })
+  }
+
+  #update() {
+    this.entities.forEach(i => {
+      i.x += i.velocity.x
+      i.y += i.velocity.y
+    })
+  }
+
+  #render() {
+    this.canvas.draw((canvas) => {
+      this.entities.forEach(i => {
+        canvas.addText(i.c, i.x, i.y, i.size)
+      })
+    })
+  }
+
+  #initWord() {
+    this.entities = this.word.split('').map((c, i) => ({
+      c: c,
+      x: this.canvas.width / 2 + i * 20,
+      y: this.canvas.height / 2,
+      s: 20,
+      velocity: {
+        x: Math.random() * this.velocityRange - this.velocityRange / 2,
+        y: Math.random() * this.velocityRange - this.velocityRange / 2,
+      },
+    }))
+  }
+}
+
+function main() {
+  const word = new URLSearchParams(window.location.search).get("w") ?? 'HELLO,WORLD!'
+  const canvas = new Canvas(
+    document,
+    document.body,
+    window.innerWidth,
+    window.innerHeight,
+  )
+
+  // run app
+  new World(canvas).run(word)
+
+  // on resize
+  window.addEventListener("resize", () => {
+    canvas.resize(window.innerWidth, window.innerHeight)
+  })
+}
+
+main()
